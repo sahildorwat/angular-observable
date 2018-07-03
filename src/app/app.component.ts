5ad53a3c4ef69f688daf8bea8090a6fd3eba126c
ssdorwat <ssdorwat@ncsu.edu>
2018-07-02 22:56:12 -0400

    added subject instead of event emitter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UsersService } from './users.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  user1Activated = false;
+  user2Activated = false;
+
+  constructor(private usersService: UsersService) {}
+
+    ngOnInit() {
+    this.usersService.userAcivated.subscribe( (number: number) => {
+        if ( number === 1) {
+          this.user1Activated = true;
+        } else if ( number === 2) {
+          this.user2Activated = true;
+        }
+    } );
+  }
+
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/Rx';
+import { Observer } from 'rxjs/Observer';
+import { Subscription } from 'rxjs/Subscription';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+    numberSubscription: Subscription;
+    customSubscription: Subscription;
 
   constructor() { }
 
   ngOnInit() {
+    const numbers = Observable.interval(1000);
+    this.numberSubscription = numbers.subscribe(
+      (number) => { console.log(number); }
+    );
+
+  const myObservable = Observable.create( ( observer: Observer<string>) => {
+    setTimeout( () => { observer.next('first package'); }, 2000);
+    setTimeout( () => { observer.next('second package'); }, 4000);
+    setTimeout( () => { observer.complete(); }, 5000);
+    setTimeout( () => { observer.error('error package'); }, 6000);
+  });
+  this.customSubscription = myObservable.subscribe( (data: string) => { console.log(data); },
+                          (error: string) => { console.log(error); },
+                          () => { console.log('completed'); },
+                        );
+
+  }
+
+  ngOnDestroy() {
+    this.numberSubscription.unsubscribe();
+    this.customSubscription.unsubscribe();
   }
 
 }
